289ee53ab4282f3e8dd0d48ec6ac2494140d0cc9
ChristianAlteri <alteri.christian@gmail.com>
2023-06-29 16:08:24 +0800

    getting post by name

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -22,4 +22,28 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+
+
+// router.update('/', async (req, res) => {
+//   try {
+//     // Get the user ID from the session
+//     const userId = req.session.user_id;
+
+//     // Create the post with the provided user ID
+//     const postData = await Post.create({
+//       ...req.body,
+//       user_id: userId,
+//     });
+//     req.session.save(() => {
+//       req.session.user_id = userId;
+//       req.session.logged_in = true;
+
+//       res.redirect('/dashboard');
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
 module.exports = router;
--- controllers/web/dashboard.js
+++ controllers/web/dashboard.js
@@ -19,4 +19,35 @@ router.get('/', (req, res) => {
     });
 });
 
+
+// Get posts of specific user by name
+// router.get('/user/:name', (req, res) => {
+//   Post.findByPk(req.params.name, {
+//     include: [{ model: Comment }, { model: User }],
+//   }).then((name) => {
+//     name = req.params 
+//     console.log(name);
+//     // console.log(data);
+//   });
+// });
+router.get('/user/:name', (req, res) => {
+  User.findOne({
+    where: { name: req.params.name },
+    include: [{ model: Post}, { model: Comment }],
+  })
+    .then((user) => {
+      res.render('userDashboard', {
+        // user: user,
+        userPosts: user.posts.map((post) => post.get({ plain: true })),
+        logged_in: req.session.logged_in,
+      });
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+
 module.exports = router;
