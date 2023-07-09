0e0915796bc9216c1a0b6c9b5a1da34354118062
ChristianAlteri <alteri.christian@gmail.com>
2023-07-09 11:29:45 +0800

    push heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD, {
@@ -17,3 +19,6 @@ const sequelize = new Sequelize(
         sequelize,
         Sequelize,
       };
+
+
+      
